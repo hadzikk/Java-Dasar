b70e2d10546305151187e8cf2b8589c0556f6a59
hadzikk <hadzikmochammad@gmail.com>
2023-08-21 15:29:05 +0700

    nine

---
+++ 24 - Do While/src/com/tutorial/Main.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+        // Do While
+        // adalah sebuah perulangan yang konsep perulanganya sama seperti while, yang dimana akan melakukan perulangan jika kondisi while bernilai true
+        // statement do akan dijalankan meskipun nilai boolean pada kondisi while false
+        // pada statement do, statement ini akan dijalankan setidaknya sekali jika kondisi while nya bernilai false
+
+        do {
+            System.out.println("hello world");
+        } while (false);
+        // output: hello wolrd sebanyak 1x
+        // catatan, do while juga memungkinkan perulangan jika dikondisikan untuk perulangan karena konsep perulanganya sama seperti while
+
+        int a = 0;
+        boolean kondisi = true;
+    }
+}
